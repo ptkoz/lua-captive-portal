0e461ba2b70dceb4c6a13857bb0bf58d856b3cf1
Patryk Kozłowski <patryk.kozlowski@toxic-software.pl>
2017-03-04 01:25:47 +0100

    Added about section.

--- application/controllers/about.lua
+++ application/controllers/about.lua
@@ -1,42 +1,13 @@
 local aboutController = require "library.controllers.controller";
 
--- login screen
-function aboutController:index()
-    if "POST" == os.getenv("REQUEST_METHOD") then
-        local params = require "library.input".parse(aboutController:fetchPostData());
-
-        if not params.token then
-            self.view.token = params.token;
-            self.view.hasError = true;
-            self.view.errorNoToken = true;
-        else
-            local token = Token.new(params.token);
-            if not token then
-                self.view.token = params.token;
-                self.view.hasError = true;
-                self.view.errorInvalidToken = true;
-            else if token.expires <= os.time() then
-                self.view.token = params.token;
-                self.view.hasError = true;
-                self.view.errorTokenExpired = true;
-            else
-                Session.create(token.token, os.getenv("REMOTE_ADDR")); -- create new session for user
-                token:expire(); -- immediately expire token
-                return self:redirect("http://auth.wro.tuxlan.es:2060/wifidog/auth?token=" .. token.token, 303, true);
-            end; end;
-        end;
-
-    end
+function aboutController:motivation()
+    self:enableBrowserCache();
+    self.view.title = "Dlaczego token zamiast hasła - FC Goście"
 end
 
--- screen after successful auth
-function aboutController:success()
-
-end
-
--- screen after failed auth
-function aboutController:error()
-
+function aboutController:geekness()
+    self:enableBrowserCache();
+    self.view.title = "Dlaczego mi się chciało - FC Goście"
 end
 
 return aboutController;
