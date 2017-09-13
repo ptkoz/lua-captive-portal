2febcb1c05310cf3bffbbbdda0f02cbfc1e92ff8
Patryk Kozłowski <patryk.kozlowski@fachowcy.pl>
2017-09-13 21:43:31 +0200

    Fixed issues with dropping database file upon deploy.

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -4,4 +4,4 @@
 SOURCE=$( greadlink -f "$(dirname "$0")/.." )
 TARGET="root@wro.tuxlan.es:/mnt/sda1/auth.wro.tuxlan.es"
 
-rsync -rlpt --delete --progress --exclude='.git*' --filter="dir-merge,- .gitignore" "$SOURCE/" "$TARGET/"
+rsync -rlpt --progress --exclude='.git*' --filter="dir-merge,- .gitignore" "$SOURCE/" "$TARGET/"
