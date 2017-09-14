f055a209bd255adacba584302cd57978fe72eaac
Patryk Kozłowski <patryk.kozlowski@fachowcy.pl>
2017-09-14 22:28:24 +0200

    Fixed token generator.

--- bin/mktoken.lua
+++ bin/mktoken.lua
@@ -6,7 +6,7 @@ local dirname = string.gsub(arg[0], "(.*)/(.*)", "%1");
 package.path = package.path .. ";" .. dirname .. "/../vendor/?.lua;" .. dirname .. "/../backend/?.lua";
 
 -- bootstrap application (to have database), bot not run
-require "application".bootstrap(dirname .. "/backend");
+require "application".bootstrap(dirname .. "/../backend");
 
 local Token = require("models.token");
 local Session = require("models.session");
