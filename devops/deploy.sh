7bb0304314760a2274e5106e62b4af72ce49d654
Patryk Kozłowski <patryk.kozlowski@fachowcy.pl>
2017-09-14 22:28:09 +0200

    Deploy only what's really needed.

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -4,4 +4,7 @@
 SOURCE=$( greadlink -f "$(dirname "$0")/.." )
 TARGET="root@wro.tuxlan.es:/mnt/sda1/auth.wro.tuxlan.es"
 
-rsync -rlpt --progress --exclude='.git*' --filter="dir-merge,- .gitignore" "$SOURCE/" "$TARGET/"
+rsync -rlpt --progress --exclude='.git*' --filter="dir-merge,- .gitignore" "${SOURCE}/backend" "${TARGET}/"
+rsync -rlpt --progress --exclude='.git*' --filter="dir-merge,- .gitignore" "${SOURCE}/bin" "${TARGET}/"
+rsync -rlpt --progress --exclude='.git*' --filter="dir-merge,- .gitignore" "${SOURCE}/gateway" "${TARGET}/"
+rsync -rlpt --progress --exclude='.git*' --filter="dir-merge,- .gitignore" "${SOURCE}/public_html" "${TARGET}/"
