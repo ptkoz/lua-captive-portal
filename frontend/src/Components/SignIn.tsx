c941e814c16c643a5fff84f6bf1c8de11b837299
Patryk Kozłowski <patryk.kozlowski@fachowcy.pl>
2017-09-15 00:53:22 +0200

    Hide instructions by default to improve speed on slower devices.

--- frontend/src/Components/SignIn.tsx
+++ frontend/src/Components/SignIn.tsx
@@ -2,20 +2,33 @@
  * Component description goes here
  */
 import * as React from "react";
-import { Col, Jumbotron, Row } from "react-bootstrap";
+import { Button, Col, Glyphicon, Jumbotron, Row } from "react-bootstrap";
 import { SignInForm } from "./form/SignInForm";
+import { PureCollapse } from "./transitions/PureCollapse";
 
 /**
  * Props available on this component
  */
-export interface ISignInProps {
+interface ISignInProps {
 
+}
+
+interface ISignInState {
+	details: boolean;
 }
 
 /**
  * The component class
  */
-export class SignIn extends React.PureComponent<ISignInProps, {}> {
+export class SignIn extends React.PureComponent<ISignInProps, ISignInState> {
+	public constructor(props) {
+		super(props);
+
+		this.state = {
+			details: false
+		};
+	}
+
 	public render(): JSX.Element {
 		return (
 			<div>
@@ -28,25 +41,37 @@ export class SignIn extends React.PureComponent<ISignInProps, {}> {
 					</div>
 				</Jumbotron>
 				<div className="container">
+					<PureCollapse show={this.state.details}>
+						<Row>
+							<Col md={4}>
+								<h2>Użyj przycisku</h2>
+								<p><img src="/images/step1.jpg" style={{ width: "100%" }} /></p>
+								<p className="text-justify">W przedpokoju obok routera znajduje się małe, białe pudełko z zielonym przyciskiem. Kiedy wciśniesz przycisk, urządzenie wygeneruje i wyświetli token przygotowany specjalnie dla Ciebie.</p>
+							</Col>
+							<Col md={4}>
+								<h2>Przepisz token</h2>
+								<p><img src="/images/step2.jpg" style={{ width: "100%" }} /></p>
+								<p className="text-justify">Przepisz wygenerowany token do formularza znajdującego się powyżej. Pośpiesz się, token wyświetla się tylko 10 sekund!</p>
+							</Col>
+							<Col md={4}>
+								<h2>Gotowe!</h2>
+								<p><img src="/images/step3.jpg" style={{ width: "100%" }} /></p>
+								<p className="text-justify">Ciesz się łatwym dostępem do internetu w naszym domu. :-)</p>
+							</Col>
+						</Row>
+					</PureCollapse>
 					<Row>
-						<Col md={4}>
-							<h2>Użyj przycisku</h2>
-							<p><img src="/images/step1.jpg" style={{ width: "100%" }} /></p>
-							<p className="text-justify">W przedpokoju obok routera znajduje się małe, białe pudełko z zielonym przyciskiem. Kiedy wciśniesz przycisk, urządzenie wygeneruje i wyświetli token przygotowany specjalnie dla Ciebie.</p>
-						</Col>
-						<Col md={4}>
-							<h2>Przepisz token</h2>
-							<p><img src="/images/step2.jpg" style={{ width: "100%" }} /></p>
-							<p className="text-justify">Przepisz wygenerowany token do formularza znajdującego się powyżej. Pośpiesz się, token wyświetla się tylko 10 sekund!</p>
-						</Col>
-						<Col md={4}>
-							<h2>Gotowe!</h2>
-							<p><img src="/images/step3.jpg" style={{ width: "100%" }} /></p>
-							<p className="text-justify">Ciesz się łatwym dostępem do internetu w naszym domu. :-)</p>
+						<Col sm={6} smOffset={3}>
+							<Button block={true} bsSize="large" onClick={this.toggleDetails}>{this.state.details ? "Ukryj" : "Pokaż"} instrukcję <Glyphicon glyph={this.state.details ? "chevron-up" : "chevron-down"} style={{ fontSize: "70%", fontWeight: 300, marginLeft: "5px" }}/></Button>
 						</Col>
 					</Row>
 				</div>
 			</div>
 		);
+	}
+
+	private toggleDetails = (event: React.MouseEvent<Button>): void => {
+		event.preventDefault();
+		this.setState({ details: !this.state.details });
 	}
 }
