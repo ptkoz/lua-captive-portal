ec0344792e1fff11101a0c8ccee17699ff1c2371
Patryk Kozłowski <patryk.kozlowski@fachowcy.pl>
2017-09-14 13:01:12 +0200

    Fixed guest guard cron checker.

--- devops/cron_check.sh
+++ devops/cron_check.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-[ -f "/etc/crontabs/root" ] && grep -q '/etc/init.d/gguard' /etc/crontabs/root && return
+if [ -f "/etc/crontabs/root" ] && grep -q '/etc/init.d/gguard' /etc/crontabs/root; then
+	echo "GuestGuard cron present."
+else
+	echo "Creating GuestGuard cron."
     echo "*/10 * * * * /etc/init.d/gguard start" >> /etc/crontabs/root
     /etc/init.d/cron start
+fi
