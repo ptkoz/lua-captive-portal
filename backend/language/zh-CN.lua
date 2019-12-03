cd39386b7c53741793ac17f10aaf79b7b99d19b8
Emil Zhai <root@derzh.com>
2019-12-03 15:00:28 +0800

    Fix some Chinese translation grammar.

--- backend/language/zh-CN.lua
+++ backend/language/zh-CN.lua
@@ -1,6 +1,6 @@
 return {
-  ["Enter the token in the field above."] = "请在上方输入框输入密码。",
-  ["The token you entered is invalid. Generate a new token."] = "密码无效，请生成一个新的密码。",
-  ["Your token has expired. Generate a new token."] = "密码过期，请生成一个新的密码。",
+  ["Enter the token in the field above."] = "请在上方输入框输入验证码。",
+  ["The token you entered is invalid. Generate a new token."] = "验证码无效，请生成一个新的验证码。",
+  ["Your token has expired. Generate a new token."] = "验证码过期，请生成一个新的验证码。",
   ["A secure connection could not be established. Please try again later."] = "无法建立安全连接，请稍后再试。",
 };
